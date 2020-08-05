f58976c4a275b236d47c5eae68811c74fec66460
Jeckill1993 <evgeniyduplenko@gmail.com>
2020-08-05 20:55:18 +0300

    create server, connect to db

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express'); //get express, read about require!
+const config = require('config'); //get config
+const mongoose = require('mongoose');
+
+const app = express()// our server
+app.use ('/api/auth', require('./routes/auth.routes')); // ?? read about it
+
+const PORT = config.get('port') || 5000 // port's number of our app
+
+async function start () {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        }) //connect with database mongoDB
+        app.listen(5000, () => console.log(`App has been started on port ${PORT}`));
+    } catch (e) {
+        console.log('Server Error', e.message);
+        process.exit(1);
+    } //read about try-catch
+}
+
+start();
+
---
+++ routes/auth.routes.js
@@ -0,0 +1,4 @@
+const {Router} = require('express');
+const  router = Router(); //create router, using func Router from express framework
+
+module.exports = router // ?? read about it
